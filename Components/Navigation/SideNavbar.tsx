14b076d8447eae70c5bd53f0b9f2f688dd86e657
Will <programergeek@gmail.com>
2023-01-19 13:56:06 +0200

    feat: create visual indicator of where you are on the navbar

--- Components/Navigation/SideNavbar.tsx
+++ Components/Navigation/SideNavbar.tsx
@@ -4,8 +4,13 @@ import { HiOutlineFolder } from "react-icons/hi";
 import { CiGrid42 } from "react-icons/ci";
 import { BsGear, BsMarkdown } from "react-icons/bs";
 import React from "react";
+import { useRouter } from "next/router";
+
+// TODO:
+// - navigate to other pages
 
 export const SideNavbar: React.FC = () => {
+  const router = useRouter();
   return (
     <Box
       sx={{
@@ -40,17 +45,71 @@ export const SideNavbar: React.FC = () => {
            * - When they are not active buttons should have a white background and a black icon outline.
            */}
           <Grid item>
-            <IconButton>
+            <IconButton
+              sx={{
+                color:
+                  router.pathname === "/" ? colours.purple[500] : undefined,
+                backgroundColor:
+                  router.pathname === "/" ? colours.purple[50] : undefined,
+                ":hover": {
+                  color:
+                    router.pathname === "/" ? colours.purple[500] : undefined,
+                  backgroundColor:
+                    router.pathname === "/" ? colours.purple[50] : undefined,
+                },
+              }}
+              onClick={() => {
+                console.log(router.pathname);
+              }}
+            >
               <CiGrid42 height={35} />
             </IconButton>
           </Grid>
           <Grid item>
-            <IconButton>
+            <IconButton
+              sx={{
+                color: /\/post\/*/.test(router.pathname)
+                  ? colours.purple[500]
+                  : undefined,
+                backgroundColor: /\/post\/*/.test(router.pathname)
+                  ? colours.purple[50]
+                  : undefined,
+                ":hover": {
+                  color: /\/post\/*/.test(router.pathname)
+                    ? colours.purple[500]
+                    : undefined,
+                  backgroundColor: /\/post\/*/.test(router.pathname)
+                    ? colours.purple[50]
+                    : undefined,
+                },
+              }}
+            >
               <HiOutlineFolder height={35} />
             </IconButton>
           </Grid>
           <Grid item>
-            <IconButton>
+            <IconButton
+              sx={{
+                color:
+                  router.pathname === "/settings"
+                    ? colours.purple[500]
+                    : undefined,
+                backgroundColor:
+                  router.pathname === "/settings"
+                    ? colours.purple[50]
+                    : undefined,
+                ":hover": {
+                  color:
+                    router.pathname === "/settings"
+                      ? colours.purple[500]
+                      : undefined,
+                  backgroundColor:
+                    router.pathname === "/settings"
+                      ? colours.purple[50]
+                      : undefined,
+                },
+              }}
+            >
               <BsGear height={35} />
             </IconButton>
           </Grid>
