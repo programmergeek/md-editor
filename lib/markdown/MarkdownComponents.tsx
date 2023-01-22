1eb7672d7f2aad9f97b1cb86ca5d1caf7ec2198d
Will <programergeek@gmail.com>
2023-01-22 19:22:23 +0200

    feat: map all header and text to mui components

--- lib/markdown/MarkdownComponents.tsx
+++ lib/markdown/MarkdownComponents.tsx
@@ -1,6 +1,7 @@
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import * as colours from "@mui/material/colors";
+import { Typography } from "@mui/material";
 
 export const MarkdownComponents = {
   code({ node, inline, className, children, ...props }: any) {
@@ -29,6 +30,56 @@ export const MarkdownComponents = {
       >
         {children}
       </code>
+    );
+  },
+  h1: ({ node, ...props }: any) => {
+    return (
+      <Typography variant="h1" sx={{ fontSize: 40, fontWeight: 600 }}>
+        {props.children}
+      </Typography>
+    );
+  },
+  h2: ({ node, ...props }: any) => {
+    return (
+      <Typography variant="h2" sx={{ fontSize: 35, fontWeight: 600 }}>
+        {props.children}
+      </Typography>
+    );
+  },
+  h3: ({ node, ...props }: any) => {
+    return (
+      <Typography variant="h3" sx={{ fontSize: 30, fontWeight: 600 }}>
+        {props.children}
+      </Typography>
+    );
+  },
+  h4: ({ node, ...props }: any) => {
+    return (
+      <Typography variant="h4" sx={{ fontSize: 25, fontWeight: 600 }}>
+        {props.children}
+      </Typography>
+    );
+  },
+  h5: ({ node, ...props }: any) => {
+    return (
+      <Typography variant="h5" sx={{ fontSize: 20, fontWeight: 600 }}>
+        {props.children}
+      </Typography>
+    );
+  },
+  h6: ({ node, ...props }: any) => {
+    return (
+      <Typography variant="h6" sx={{ fontSize: 15, fontWeight: 600 }}>
+        {props.children}
+      </Typography>
+    );
+  },
+  p: ({ node, ...props }: any) => {
+    return (
+      <Typography sx={{ fontSize: { xs: 12, lg: 16 } }}>
+        {" "}
+        {props.children}{" "}
+      </Typography>
     );
   },
 };
