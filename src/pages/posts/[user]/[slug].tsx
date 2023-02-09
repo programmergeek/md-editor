50f43aab1f59d6e0482071d6c784cae612145a28
Will <programergeek@gmail.com>
2023-02-09 16:09:02 +0200

    feat: add placeholder page title

--- src/pages/posts/[user]/[slug].tsx
+++ src/pages/posts/[user]/[slug].tsx
@@ -165,7 +165,7 @@ const Post: React.FC = ({
       />
       <Layout>
         <Head>
-          <title>{title}</title>
+          <title>{title ? title : "Untitled"}</title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
         <Container>
